b2cb218078fcc525687cf7da66ebfa41ecd3e729
WilliamParsons <wdp0005@uah.edu>
2015-02-10 09:23:19 -0600

    Bubblesort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int n = 10;
+	char arrayOfValues[100];
+	char *mediary = NULL;
+	int temp = 0;
+	int count = 0;
+	ifstream myInputFile;
+	myInputFile.open("input.txt");
+	if (myInputFile.is_open())
+	{
+		while (myInputFile.good())
+		{
+				temp = myInputFile.get();
+				*mediary = char(temp);
+				if(temp != ',')
+				{
+				
+				arrayOfValues[i] = atoi(mediary);
+				cout<<arrayOfValues[i];
+				i++;
+				count++;
+				}
+		}
+		myInputFile.close();
+	}
+
+	else cout << "Unable to open file"; 
+
+
+
+	for (j; j <= 10; j++)
+	{
+		for(k; k <= n- 2 - j; k++)
+		{
+			if (arrayOfValues[k+1] < arrayOfValues[k])
+			{
+				if(arrayOfValues != NULL)
+				{
+					temp = arrayOfValues[k];
+					arrayOfValues[k] = arrayOfValues[k+1];
+					arrayOfValues[k+1] = temp;
+				}
+			}
+
+		}
+
+
+	}
+	i = 0;
+	ofstream myOutputFile ("output.txt");
+	for(i; i <= count; i++)
+	{
+		myOutputFile<<arrayOfValues[i]<<" , "<<endl;
+
+	}
+	myOutputFile.close();
+}
